09a912e6fd820698a4ea2a9039db06efee4f4c51
Damiano Dionori <107266901+damianodionori@users.noreply.github.com>
2023-11-08 20:26:58 +0000

    buttons

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,30 +1,57 @@
-import React from "react";
-import { useState, useEffect } from "react";
-
+import React, { useState, useEffect } from "react";
 import SimpleCounter from "./simplecounter";
 
-//include images into your bundle
-
-//create your first component
 const Home = () => {
-	const [counter, setCounter] = useState(1233); // Set the initial count here
-  
-	useEffect(() => {
-	  const intervalId = setInterval(() => {
-		setCounter((prevCounter) => prevCounter - 1); // Counting down
-	  }, 1000);
-  
-  
-	  return () => {
-		clearInterval(intervalId);
-	  };
-	}, []);
-  
-	return (
-		<div>
-		  <SimpleCounter initialCount={counter} /> {/* Pass the initial count */}
-		</div>
-	);
-  }
-  
-  export default Home;  
+  const [counter, setCounter] = useState(60); // Set the initial count here
+  const [isRunning, setIsRunning] = useState(true);
+
+  const handleStart = () => {
+    setIsRunning(true);
+  };
+
+  const handleStop = () => {
+    setIsRunning(false);
+  };
+
+  const handleReset = () => {
+    setIsRunning(true);
+    setCounter(60); // Set the initial count here
+  };
+
+  useEffect(() => {
+    let intervalId;
+
+    if (isRunning) {
+      intervalId = setInterval(() => {
+        setCounter((prevCounter) => prevCounter - 1); // Counting down
+      }, 1000);
+    } else {
+      clearInterval(intervalId);
+    }
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [isRunning]);
+
+  return (
+    <div>
+      <SimpleCounter initialCount={counter} />
+      <div className="text-center mt-1">
+        <div className="btn-group" role="group">
+          <button className="btn btn-primary" onClick={handleStart} disabled={isRunning}>
+            Resume
+          </button>
+          <button className="btn btn-secondary" onClick={handleStop} disabled={!isRunning}>
+            Stop
+          </button>
+          <button className="btn btn-danger" onClick={handleReset}>
+            Reset
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
